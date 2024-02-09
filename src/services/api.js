5d1802b98813667ecc0dfbc22c1da93d7ac7e6f7
Miguel <miguel.giannotti07@outlook.com>
2024-02-09 17:37:26 -0300

    voltei

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,4 @@
 import axios from 'axios'
-import express from 'express'
-
-const app = express()
-
-app.use(function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://code-burger-interface-fptrs4u79-miguelgt1s-projects.vercel.app',
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  next()
-})
 
 const apiCodeBurger = axios.create({
   baseURL: 'https://code-club-burger-production-ba1f.up.railway.app/',
