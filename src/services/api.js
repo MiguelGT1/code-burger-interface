feeba72244122145d754c21bfcb9935f3bba6c9c
Miguel <miguel.giannotti07@outlook.com>
2024-02-17 19:14:33 -0300

    api confeitaria

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,14 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://code-club-burger-production-ba1f.up.railway.app/',
 })
 
-// Interceptores de requisição
 apiCodeBurger.interceptors.request.use(async config => {
   const userData = await localStorage.getItem('codeburger:userData')
   const token = userData && JSON.parse(userData).token
   config.headers.Authorization = `Bearer ${token}`
   return config
 })
-
-// const apiCodeBurger = axios.create({
-// 'https://code-club-burger-production-ba1f.up.railway.app/',
-// })
-
-// baseURL: apiCodeBurger.interceptors.request.use(async config => {
-// const userData = await localStorage.getItem('codeburger:userData')
-// const token = userData && JSON.parse(userData).token
-// config.headers.Authorization = `Bearer ${token}`
-// return config
-// })
 
 export default apiCodeBurger
